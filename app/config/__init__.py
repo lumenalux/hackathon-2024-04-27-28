888554100ca4a01a4df4f1c62ef49d2c4b2cbcde
lumenalux <stefanmartinuk@gmail.com>
2024-04-28 22:52:36 +0300

    Remove commented code

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,28 +1,6 @@
 from dotenv import load_dotenv
 import os
 
-
-# class Config:
-#     JWT_SECRET_KEY = 'change-me-fd0e02667fe62206a40ed9d1c75b0d8a'
-#     SECRET_KEY = 'change-me-bea091d14d4247a694461b1816e13b'
-
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
-
-#     MAIL_SERVER = 'smtp-relay.brevo.com'
-#     MAIL_PORT = 587
-#     MAIL_USERNAME = 'stefanmartinuk@gmail.com'
-#     MAIL_PASSWORD = 'XYC8jAQMw2D05mkK'
-#     MAIL_USE_TLS = True
-#     MAIL_USE_SSL = False
-
-
-# class OAuthConfig:
-#     GOOGLE_CLIENT_ID = '60844738170-egvfjsn6201f21qa4jdm2rja61h2j3cq.apps.googleusercontent.com'  # noqa
-#     GOOGLE_CLIENT_SECRET = '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'
-#     GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'  # noqa
-
-
-# Load environment variables from .env file
 load_dotenv()
 
 
